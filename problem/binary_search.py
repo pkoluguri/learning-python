29453e6df156958c709c83f97cd97a3d96dd676a
Manas Pottlapelly <Manas@MURALIs-MacBook-Air.local>
2021-05-08 12:40:23 +0530

    Modified Binary Search

--- problem/binary_search.py
+++ problem/binary_search.py
@@ -1,19 +1,24 @@
-binary_search_list = [1,2,3,4,5,6,7]
+binary_search_list = [1,2,3,4,5,6]
 binary_search_list.sort()
 
-def perform_binary_search(start:int,stop:int,number:int):
-  mid = start+(stop-start)//2
-  if stop >= start:
-   if binary_search_list[mid] == number:
+def perform_binary_search(begin:int, end:int , search:int):
+  mid = begin+(end-begin)//2
+  if end >= begin:
+   if binary_search_list[mid] == search:
+
      return mid
-   elif number > binary_search_list[mid]:
-     return perform_binary_search(mid+1,stop,number)
-   elif number < binary_search_list[mid]:
-     return perform_binary_search(1,mid-1,number)
+
+   elif search > binary_search_list[mid]:
+
+     return perform_binary_search(mid+1,end,search)
+
+   elif search < binary_search_list[mid]:
+
+     return perform_binary_search(0,mid-1,search)
+
   else:
       return None
 
 if __name__ == "__main__":
-    result= perform_binary_search(0,len(binary_search_list)-1,10)
+    result= perform_binary_search(0,len(binary_search_list)-1,5)
     print(f"number at index {result}")
-    
